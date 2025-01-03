5b70dfbf4b38af5b06cd681369cd602ceb626d23
branttorious <57111974+branttorious@users.noreply.github.com>
2025-01-03 11:43:31 -0500

    Update env.sh

--- configs/env.sh
+++ configs/env.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Environment variables for automation scripts
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export BACKUP_DIR="~/automation/backups"
+export LOG_DIR="~/automation/logs"
+export DB_USER="your_database_user"
+export DB_PASS="your_database_password"
 
